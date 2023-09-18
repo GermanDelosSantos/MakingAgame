37b714c2e92d2d8a1c70f2e8e49285bb1766aa6d
German De los Santos <noiconuf@gmail.com>
2023-09-18 20:58:21 -0300

    se trabajo en darle estilo al juego y en la mecanica de achicar a los enemigos por golpe

--- index.js
+++ index.js
@@ -65,15 +65,39 @@ class Enemy {
         this.y = this.y + this.velocity.y
     }
 }
+//Creacion de Particulas
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'red')
+const player = new Player(x, y, 10, 'white')
 
 // const para animaciones
 const projetiles = []
 const enemies = []
+const particles = []
 // spawn de enemigos
 function spawnEnemies() {
     setInterval(() =>{
@@ -92,7 +116,7 @@ function spawnEnemies() {
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
 
         }
-        const color = 'black'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
         const angle = Math.atan2(canvas.height / 2 - y,canvas.width / 2 - x)
         const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
@@ -106,8 +130,12 @@ function spawnEnemies() {
 let animationId
 function animate () {
     animationId = requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
+    particles.forEach(particle =>{
+        particle.update()
+    });
     projetiles.forEach((projectile, index) => {
         projectile.update()
         // Sacar projectiles fuera del juego
@@ -136,12 +164,25 @@ function animate () {
 
             if (dist - enemy.radius - projectile.radius < 1) 
             {
-                setTimeout(() => {
-                    enemies.splice(index, 1)
-                    projetiles.splice(projectileIndex, 1)
-
-                }, 0)
-
+                for (let i = 0; i < 8; i++) {
+                    particles.push (new Particle(projectile.x, projectile.y, 3, enemy.color,
+                        {x: Math.random() - 0.5, y: Math.random() - 0.5}));
+                    
+                }
+                if(enemy.radius -10 > 5){
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    })
+                    setTimeout(() => {
+                        projetiles.splice(projectileIndex, 1)
+                    }, 0)
+                } else{
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projetiles.splice(projectileIndex, 1)
+    
+                    }, 0)
+                }
             }
         })
     });
@@ -150,11 +191,11 @@ function animate () {
 addEventListener('click', (event) =>
     {
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
-    const velocity = {x: Math.cos(angle), y: Math.sin(angle)
+    const velocity = {x: Math.cos(angle) * 5, y: Math.sin(angle) * 5
     } 
     console.log (angle)
     projetiles.push(
-        new Projectile(canvas.width/2, canvas.height / 2, 5, 'blue', velocity)
+        new Projectile(canvas.width/2, canvas.height / 2, 5, 'white', velocity)
     )
 })
 
